82eb161405de76466a3b4db842292e82c0733700
sergiosanzllamas <sanzsergio4ab@gmail.com>
2019-01-14 20:46:48 +0100

    Realizada interfaz de inicio
    Prueba

---
+++ Proyecto/app/src/main/java/com/example/usuario/proyecto/Splah.java
@@ -0,0 +1,25 @@
+package com.example.usuario.proyecto;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splah);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+          Intent intent = new Intent(Splah.this,MainActivity.class);
+          startActivity(intent);
+            }
+        },4000);
+    }
+}
