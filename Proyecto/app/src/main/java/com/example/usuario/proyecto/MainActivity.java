02872ee228d41ded3c2e79da3a28e27fc0b756d1
AdrianGCS <adrian1271997@gmail.com>
2019-01-16 00:38:59 +0100

    Añadida Libreria para Conectarse a MySql y intento de conexión
    Actualmente la conexión falla , no envia la consulta

--- Proyecto/app/src/main/java/com/example/usuario/proyecto/MainActivity.java
+++ Proyecto/app/src/main/java/com/example/usuario/proyecto/MainActivity.java
@@ -3,18 +3,43 @@ package com.example.usuario.proyecto;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.blikoon.qrcodescanner.QrCodeActivity;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_QR_SCAN = 101;
+    private static final String url ="jdbc:mysql://192.168.1.12:3306/proyecto";
+    private static final String user = "Consultas";
+    private static final String pass = "Consultas";
+    private TextView Datos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Datos = (TextView) findViewById(R.id.Datos);
+        Button Conectar =(Button) findViewById(R.id.Conectar);
+
+        Conectar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MyTask().execute();
+            }
+        });
     }
     public void onClick(View v) {
         Intent i = new Intent(MainActivity.this, QrCodeActivity.class);
@@ -36,6 +61,47 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Leído: " + lectura, Toast.LENGTH_SHORT).show();
 
             }
+        }
+    }
+    private class MyTask extends AsyncTask<Void, Void, Void>{
+        private String fName="";
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url,user,pass);
+
+                Statement st = con.createStatement();
+                String sql ="SELECT * FROM `enfermo`";
+
+                final ResultSet rs = st.executeQuery("SELECT * FROM `enfermo`");//revisar esto , el servidor no lo ejecuta
+                rs.next();
+                fName=rs.getNString(1);
+                //estas lineas son de prueba , nunca se llegan a ejecutar en teoria
+                if(fName==null){
+                    fName=rs.getNString(2);
+                    if(fName==null){
+                        fName=rs.getNString(3);
+                        if(fName==null){
+                            fName=rs.getNString("Error");
+                        }
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+            Datos.setText(fName);
+            //Pruebas
+            if(Datos==null){
+                Datos.setText("Error");
+            }
+            super.onPostExecute(result);
         }
     }
 }
