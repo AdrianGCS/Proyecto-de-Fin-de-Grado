9433a3f50ac72ea4695bf00936f0c138c62d7ff7
sergiosanzllamas <sanzsergio4ab@gmail.com>
2019-01-15 20:29:15 +0100

    Lector QR
    NO sale la camara

--- Proyecto/app/src/main/java/com/example/usuario/proyecto/MainActivity.java
+++ Proyecto/app/src/main/java/com/example/usuario/proyecto/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.usuario.proyecto;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
+import com.blikoon.qrcodescanner.QrCodeActivity;
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REQUEST_CODE_QR_SCAN = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClick(View v) {
+        Intent i = new Intent(MainActivity.this, QrCodeActivity.class);
+        startActivityForResult(i, REQUEST_CODE_QR_SCAN);
+    }
+   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode != Activity.RESULT_OK) {
+            Toast.makeText(getApplicationContext(), "No hay respuesta", Toast.LENGTH_SHORT).show();
+            String resultado = data.getStringExtra("com.blikoon.qrcodescanner.error_decoding_image");
+            if (resultado != null) {
+                Toast.makeText(getApplicationContext(), "Codigo QR desconocido", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        if (requestCode == REQUEST_CODE_QR_SCAN) {
+            if (data != null) {
+                String lectura = data.getStringExtra("com.blikoon.qrcodescanner.got_qr_scan_relult");
+                Toast.makeText(getApplicationContext(), "Leído: " + lectura, Toast.LENGTH_SHORT).show();
+
+            }
+        }
     }
 }
