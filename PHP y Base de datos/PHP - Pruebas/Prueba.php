0f3f5bd922d7f9166d70b0f54e1655026c4b50b8
AdrianGCS <adrian1271997@gmail.com>
2019-02-07 00:46:27 +0100

    Pruebas de conexión mediante PHP

---
+++ PHP y Base de datos/PHP - Pruebas/Prueba.php
@@ -0,0 +1,21 @@
+﻿<?php
+//--Datos del servidor mysql
+$conn = new mysqli("localhost", "Consultas", "Consultas", "proyecto");
+//--Arreglo
+$json = array();
+// Verificar conexión
+echo "[";
+if ($conn->connect_error) {
+   //--Cuando la conexion es fallida!
+    $json['conexion']= "Conexion Fallida!";
+} else{
+   //--Cuando la conexión es exitosa!
+   $json['conexion']  = "Conexion Exitosa!";
+}
+
+//--Imprime el mensaje resultante.
+echo json_encode($json);
+echo "]";
+//--Cierra conexion
+$conn->close();
+?>
