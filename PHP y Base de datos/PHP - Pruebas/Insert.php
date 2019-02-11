cccffdb999650ed19c5133f281ce73f281fe14da
AdrianGCS <adrian1271997@gmail.com>
2019-02-11 18:52:07 +0100

    Archivos de PHP

---
+++ PHP y Base de datos/PHP - Pruebas/Insert.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+	
+	// Datos de la base de datos
+	$usuario = "Consultas";
+	$password = "Consultas";
+	$servidor = "localhost";
+	$basededatos = "proyecto";
+	
+
+	$conexion = mysqli_connect( $servidor, $usuario,$password ) or die ("No se ha podido conectar al servidor de Base de datos");
+	
+
+	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+
+	$consulta = "INSERT INTO `usuario` (`ID`, `Nombre`, `Apellido`) VALUES (NULL, 'prueba', 'txr');";
+
+	mysqli_close( $conexion );
+?>
---
+++ PHP y Base de datos/PHP - Pruebas/Update.php
@@ -0,0 +1,23 @@
+﻿ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "proyecto";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "UPDATE usuario SET Apellido='Doe' WHERE id=2";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+?> 
