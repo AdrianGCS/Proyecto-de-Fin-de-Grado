062ac171f8212152489af26e35ea3afbdd5b9599
sergiosanzllamas <sanzsergio4ab@gmail.com>
2019-03-15 09:12:56 +0100

    Mapa prueba

---
+++ Mapa/app/src/main/java/com/example/mapa/MainActivity.java
@@ -0,0 +1,271 @@
+package com.example.mapa;
+
+import android.annotation.SuppressLint;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.location.LocationEnginePriority;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationEngineListener, PermissionsListener, MapboxMap.OnMapClickListener {
+private MapView mapView;
+private MapboxMap map;
+private Button boto;
+private PermissionsManager permissionsManager;
+private LocationEngine locationEngine;
+private LocationLayerPlugin locationLayerPlugin;
+private Location originLocation;
+private Point originPosition;
+private Point destinationPosition;
+private Marker marker;
+private NavigationMapRoute navigationMapRoute;
+private static final String TAG="MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this,getString(R.string.access_token));
+        setContentView(R.layout.activity_main);
+        mapView=(MapView)findViewById(R.id.mapav);
+        mapView.onCreate(savedInstanceState);
+        boto=findViewById(R.id.boton);
+        mapView.getMapAsync(this);
+        //setRequestedOrientation(MainActivity.SCREEN_ORIENTATION_PORTRAIT);
+        boto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavigationLauncherOptions options= NavigationLauncherOptions.builder()
+                        .origin(originPosition)
+                        .destination(destinationPosition)
+                        .shouldSimulateRoute(false)
+                        .build();
+                NavigationLauncher.startNavigation(MainActivity.this,options);
+
+
+                {
+                }
+            }
+        });
+
+    }
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        map=mapboxMap;
+        map.addOnMapClickListener(this);
+        enableLocation();
+    }
+    private void enableLocation(){
+        if(PermissionsManager.areLocationPermissionsGranted(this)){
+            inicializalocaengine();
+            inicializalocalayer();
+        }else{
+            permissionsManager=new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+    @SuppressWarnings("MissingPermission")
+    protected void inicializalocaengine(){
+    locationEngine=new LocationEngineProvider(this).obtainBestLocationEngineAvailable();
+    locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+    locationEngine.activate();
+
+    Location lastlocation=locationEngine.getLastLocation();
+    if(lastlocation !=null){
+        originLocation=lastlocation;
+        setCameraPosicion(lastlocation);
+
+    }else{
+        locationEngine.addLocationEngineListener(this);
+    }
+    }
+    @SuppressWarnings("MissingPermission")
+    private void inicializalocalayer(){
+    locationLayerPlugin=new LocationLayerPlugin(mapView,map,locationEngine);
+    locationLayerPlugin.setLocationLayerEnabled(true);
+    locationLayerPlugin.setCameraMode(CameraMode.TRACKING);
+    locationLayerPlugin.setRenderMode(RenderMode.NORMAL);
+    }
+
+    private void setCameraPosicion(Location location){
+    map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()),13.0));
+
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng point) {
+        if(marker !=null){
+            map.removeMarker(marker);
+        }
+        marker= map.addMarker(new MarkerOptions().position(point));
+        destinationPosition=Point.fromLngLat(point.getLongitude(),point.getLatitude());
+        originPosition=Point.fromLngLat(originLocation.getLongitude(),originLocation.getLatitude());
+        getRoute(originPosition,destinationPosition);
+        boto.setEnabled(true);
+        boto.setBackgroundResource(R.color.mapbox_blue);
+    }
+    private void getRoute(Point origen,Point destino){
+        NavigationRoute.builder()
+                .accessToken(Mapbox.getAccessToken())
+                .origin(origen)
+                .destination(destino)
+                //.voiceUnits(DirectionsCriteria.METRIC)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        if(response.body()== null){
+                            Log.e(TAG,"NO RUTAS, NO VALEN");
+                            return;
+                        }else if(response.body().routes().size()== 0){
+                            Log.e(TAG,"NO he encontrado rutas");
+                            return;
+                        }
+
+                        DirectionsRoute directionsRoute=response.body().routes().get(0);
+                        if(navigationMapRoute !=null){
+                            navigationMapRoute.removeRoute();
+                        }else{
+                            navigationMapRoute=new NavigationMapRoute(null,mapView,map);
+                        }
+
+                        navigationMapRoute.addRoute(directionsRoute);
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                        Log.e(TAG,"ERROR:"+t.getMessage());
+
+                    }
+                });
+    }
+
+    @Override
+    @SuppressWarnings("MissingPermission")
+    public void onConnected() {
+locationEngine.requestLocationUpdates();
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location !=null){
+    originLocation=location;
+    setCameraPosicion(location);
+}
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if(granted){
+         enableLocation();
+         
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        permissionsManager.onRequestPermissionsResult(requestCode,permissions,grantResults);
+    }
+
+    @SuppressWarnings("MissingPermission")
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(locationEngine !=null){
+           locationEngine.requestLocationUpdates();
+        }
+        if(locationLayerPlugin !=null){
+            locationLayerPlugin.onStart();
+        }
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(locationEngine !=null){
+            locationEngine.removeLocationUpdates();
+        }
+        if(locationLayerPlugin !=null){
+            locationLayerPlugin.onStop();
+        }
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(locationEngine !=null){
+            locationEngine.deactivate();
+        }
+        mapView.onDestroy();
+    }
+
+
+
+
+}
