faf5bf710cc810ead91f51ae721de2c552a6f912
sergiosanzllamas <sanzsergio4ab@gmail.com>
2019-04-09 17:44:22 +0200

    Revert "Lector"
    This reverts commit 8a53038012aaf6ea3164eeb4b22003aac6c7211d.

--- Proyecto/app/src/main/java/com/example/usuario/proyecto/EscanerQr.java
+++ Proyecto/app/src/main/java/com/example/usuario/proyecto/EscanerQr.java
@@ -1,8 +1,5 @@
 package com.example.usuario.proyecto;
 
-import android.app.ProgressDialog;
-import android.graphics.Bitmap;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.Manifest;
@@ -18,44 +15,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
-import org.json.JSONObject;
-
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class EscanerQr extends AppCompatActivity {
     SurfaceView surfaceView;
-    TextView texto;
+    TextView txtBarcodeValue;
     private BarcodeDetector barcodeDetector;
     private CameraSource cameraSource;
     private static final int REQUEST_CAMERA_PERMISSION = 201;
     Button btnAction;
-    public static String intentData;
-    private AccessServiceAPI miservicio;
-    private ProgressDialog dialogo;
+    String intentData = "";
+    boolean isEmail = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escaner_qr);
         initViews();
-        miservicio = new AccessServiceAPI();
     }
-
     private void initViews() {
-        texto = findViewById(R.id.txtBarcodeValue);
+        txtBarcodeValue = findViewById(R.id.txtBarcodeValue);
         surfaceView = findViewById(R.id.surfaceView);
         btnAction = findViewById(R.id.btnAction);
         btnAction.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(EscanerQr.this, Principal.class));
+                startActivity(new Intent(EscanerQr.this,Principal.class));
                /* if (intentData.length() > 0) {
 
                         startActivity(new Intent(EscanerQr.this,Principal.class));
@@ -68,7 +57,6 @@ public class EscanerQr extends AppCompatActivity {
             }
         });
     }
-
     private void initialiseDetectorsAndSources() {
 
         Toast.makeText(getApplicationContext(), "LECTOR EMPEZADO", Toast.LENGTH_SHORT).show();
@@ -82,6 +70,7 @@ public class EscanerQr extends AppCompatActivity {
                 .setAutoFocusEnabled(true)
 
                 .build();
+        
 
 
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
@@ -103,100 +92,65 @@ public class EscanerQr extends AppCompatActivity {
             }
 
 
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                cameraSource.stop();
-            }
-        });
-
-        class TaskLogin extends AsyncTask<String, Void, Integer> {
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                //Open progress dialog during login
-                dialogo = ProgressDialog.show(EscanerQr.this, "Porfavor, Espere", "Procesando", true);
-            }
-            @Override
-            protected Integer doInBackground(String... params) {
-                //Create data to pass in param
-                Map<String, String> param = new HashMap<>();
-                param.put("action", "login");
-                param.put("datos", params[1]);
-
-
-                JSONObject jObjResult;
-                try {
-
-                    jObjResult = miservicio.convertJSONString2Obj(miservicio.getJSONStringWithParam_POST(Common.SERVICE_API_URL, param));
-                    intentData=jObjResult.getString("datos");
-                    return jObjResult.getInt("result");
-                } catch (Exception e) {
-                    return Common.RESULT_ERROR;
-                }
-            }
-
-
-            @Override
-            protected void onPostExecute(Integer result) {
-                super.onPostExecute(result);
-                dialogo.dismiss();
-                if (Common.RESULT_SUCCESS == result) {
-                    Toast.makeText(getApplicationContext(), "Login Correcto", Toast.LENGTH_LONG).show();
-                    Intent i = new Intent(getApplicationContext(), OpcionUser.class);
-                    i.putExtra("datos",texto.getText().toString());
-                    startActivity(i);
-                } else {
-                    Toast.makeText(getApplicationContext(), "Login Mal", Toast.LENGTH_LONG).show();
-                }
-            }
-        }
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-                Toast.makeText(getApplicationContext(), "DETENIDO EL USO DE MEMORIA DEL TELEFONO", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
-                if (barcodes.size() != 0) {
-
-
-                    texto.post(new Runnable() {
-
-                        @Override
-                        public void run() {
-
-                            if (barcodes.valueAt(0).email != null) {
-                                texto.removeCallbacks(null);
-                                btnAction.setText("Coge la URL");
-                                intentData = barcodes.valueAt(0).displayValue;
-                                texto.setText(intentData);
-
-                            }
-                        }
-                    });
-
-                }
-            }
-        });
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
     }
 
-
     @Override
-    protected void onPause() {
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        cameraSource.stop();
+    }
+});
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+@Override
+public void release() {
+        Toast.makeText(getApplicationContext(), "DETENIDO EL USO DE MEMORIA DEL TELEFONO", Toast.LENGTH_SHORT).show();
+        }
+
+@Override
+public void receiveDetections(Detector.Detections<Barcode> detections) {
+final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+        if (barcodes.size() != 0) {
+
+
+        txtBarcodeValue.post(new Runnable() {
+
+@Override
+public void run() {
+
+        if (barcodes.valueAt(0).email != null) {
+        txtBarcodeValue.removeCallbacks(null);
+        intentData = barcodes.valueAt(0).email.address;
+        txtBarcodeValue.setText(intentData);
+        isEmail = true;
+        btnAction.setText("AÑADE");
+        } else {
+        isEmail = false;
+        btnAction.setText("Coge la URL");
+        intentData = barcodes.valueAt(0).displayValue;
+        txtBarcodeValue.setText(intentData);
+
+        }
+        }
+        });
+
+        }
+        }
+        });
+        }
+
+
+@Override
+protected void onPause() {
         super.onPause();
         cameraSource.release();
-    }
+        }
 
-    @Override
-    protected void onResume() {
+@Override
+protected void onResume() {
         super.onResume();
         initialiseDetectorsAndSources();
-    }
-
+        }
 
 }
