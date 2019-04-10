9fb14d79a28c503bd1962f1411f7aa676a08132a
AdrianGCS <adrian1271997@gmail.com>
2019-04-10 21:48:28 +0200

    Archivo PHP

--- PHP y Base de datos/PHP/PDOConnection.php
+++ PHP y Base de datos/PHP/PDOConnection.php
@@ -3,4 +3,4 @@ $dbName = "proyecto";
 $user = "root";
 $pwd = "";
 $host = "localhost";
-$cnn = new mysqli("localhost","root", "", "proyecto");
+$cnn = new mysqli("localhost","publico", "oCgz4GjoDw3zNscX", "proyecto");
--- PHP y Base de datos/PHP/user.php
+++ PHP y Base de datos/PHP/user.php
@@ -1,7 +1,8 @@
 <?php
 include("PDOConnection.php");
 
-//Define some value
+
+
 define("ACTION_ADD_USER", "add");
 define("ACTION_LOGIN", "login");
 define("ACTION_ENFERMO", "enfermo");
@@ -9,63 +10,95 @@ define("RESULT_SUCCESS", 0);
 define("RESULT_ERROR", 1);
 define("RESULT_USER_EXISTS", 2);
 
-
 $action = $_POST["action"];
 $result = RESULT_ERROR;
+$Encript= 8;
 
+$id = 2;
+$idEnfermo = 0;
 
-if(isset($action))
-{
-	$username = $_POST["username"];
-	$mail = $_POST["mail"];
-	$pwd = $_POST["password"];
-	$lastname = $_POST["lastname"];
-	$id = 0;
+switch ($action) {
 
-	if(ACTION_ADD_USER == $action)
-	{
-		//Check exists user
+	case "add":
+
+		$username = $_POST["username"];
+		$lastname = $_POST["lastname"];
+		$mail = $_POST["mail"];
 		if(isExistUser($cnn, $mail))
 		{
 			$result = RESULT_USER_EXISTS;
 		}
 		else
-		{
+		{	
+
+			$mail = $_POST["mail"];
+			$pwd = $_POST["password"];
 			$id=insertUser($cnn, $username, $lastname);
 			insertCuenta($cnn, $mail, $pwd, $id);
 			$result = RESULT_SUCCESS;
 		}
-	}
-	else{
-		if(ACTION_ENFERMO == $action)
-		{
-				$phone = $_POST["phone"];
-				$adress = $_POST["adress"];
-				$id=insertEnfermo($cnn, $id, $username ,$lastname, $phone, $adress );
-				$result = RESULT_SUCCESS;
-			}
+
+		break;
+
+		case "enfermo":
+
+		$username = $_POST["username"];
+		$lastname = $_POST["lastname"];
+		$phone = $_POST["phone"];
+		$adress = $_POST["adress"];
+		if (is_null($adress)) {
+
+			$adress = "";
+			$idEnfermo=insertEnfermo($cnn, $id, $username ,$lastname, $phone, $adress );
+
+		}
 		else{
 
+		$idEnfermo=insertEnfermo($cnn, $id, $username ,$lastname, $phone, $adress );
+		
+		}
+
+		$data = $idEnfermo . '/' . $username. '/' . $lastname . '/' . $phone;
+				
+		$Encript= openssl_encrypt($data, 'aes128', 'Seguridad',0,'1234567812345678');
+
+		$result = RESULT_SUCCESS;
+
+		break;
+	
+
+		case "login":
+
+			$mail = $_POST["mail"];
+			$pwd = $_POST["password"];
 			if(login($cnn, $mail, $pwd))
 			{
+				$id=loginId($cnn, $mail, $pwd);
 				$result = RESULT_SUCCESS;
 			}
 			else
 			{
 				$result = RESULT_ERROR;
 			}
-		}
-	}
+
+		break;
+
+		case "qr":
+			$qr = $_POST["qr"];
+			$Encript= openssl_decrypt($qr, 'aes128', 'Seguridad',0,'1234567812345678');
+			break;
+
 }
+
 //Print result as json
-echo(json_encode(array('result' => $result , 'id' => $id)));
+echo(json_encode(array('result' => $result , 'id' => $id , 'Encriptado' => $Encript)));
 
 function insertEnfermo($cnn, $id, $username ,$lastname, $phone, $adress )
 {
 
-	$query = "INSERT INTO CUENTA(ID_USUARIO , NOMBRE, APELLIDO, TELEFONO_CONTACTO, DIRECCION) VALUES(?, ?, ?, ?, ?)";
+	$query = "INSERT INTO ENFERMO(ID_USUARIO , NOMBRE, APELLIDO, TELEFONO_CONTACTO, DIRECCION) VALUES(?, ?, ?, ?, ?)";
 	$stmt = $cnn->prepare($query);
-	$stmt->bind_param("issis", $id, $username ,$lastname, $phone, $adress );
+	$stmt->bind_param("issis", $id, $username ,$lastname, $phone, $adress);
 	$stmt->execute();
 	return $cnn->insert_id;
 
@@ -89,6 +122,7 @@ function insertUser($cnn, $username, $lastname)
 	$stmt->bind_param("ss", $username, $lastname);
 	$stmt->execute();
 	return $cnn->insert_id;
+
 	
 
 }
@@ -116,3 +150,19 @@ function login($cnn, $mail, $pwd)
 	return $rowcount;
 
 }
+
+function loginId($cnn, $mail, $pwd)
+{
+	$query = "SELECT ID_USUARIO FROM CUENTA WHERE CORREO = ? AND CONTRASENIA = ?";
+	$stmt = $cnn->prepare($query);
+	$stmt->bind_param("ss", $mail , $pwd);
+	$stmt->bind_result($id);
+	$stmt->execute();
+	$stmt->store_result();
+	$stmt->fetch();
+	
+	return $id;
+
+
+}
+
