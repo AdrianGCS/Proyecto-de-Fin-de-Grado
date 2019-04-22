718e545bcee71cb88f579450782dc103eee395cf
AdrianGCS <adrian1271997@gmail.com>
2019-04-22 17:25:13 +0200

    Condiciones

--- Proyecto/app/src/main/java/com/example/usuario/proyecto/Condiciones.java
+++ Proyecto/app/src/main/java/com/example/usuario/proyecto/Condiciones.java
@@ -8,17 +8,24 @@ import android.content.*;
 import android.widget.Button;
 
 public class Condiciones extends AppCompatActivity {
-Button button;
+    Button button;
+    Button end;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_condiciones);
+
         button=findViewById(R.id.acceptar);
+        end=findViewById(R.id.Salir);
+
+        button.setOnClickListener(this::onClick);
+        end.setOnClickListener(this::onClick);
     }
     public void onClick(View v) {
 
+        switch (v.getId()){
 
-
+            case R.id.acceptar:
                 startActivity(new Intent(Condiciones.this, MainActivity.class));
 
                 SharedPreferences prefs =
@@ -27,6 +34,13 @@ Button button;
                 editor.putInt("Estado",1);
 
                 editor.commit();
+                break;
+
+            case R.id.Salir:
+                finish();
+                break;
+
+        }
 
 
 
